d02f09448dfeb8251d607388a91735fa90c4d4ba
includeMaple <maple_dby@163.com>
2023-02-18 14:09:27 +0800

    阶段性完成

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,39 @@
 import { Rules } from './core/rules';
 import { Token } from './core/token';
+import { TokenTree } from './core/tree';
 import { Render } from './core/render';
-import { ITokenItem } from './core/iface';
+import { toolbox } from './utils';
 export class MdC {
   public rule: Rules = new Rules();
   private token: Token = new Token();
   private render: Render = new Render();
+  private tree: TokenTree = new TokenTree();
 
   constructor () {}
 
   exec (document: string) {
+    
     this.rule.generate();
+    
     this.token.setRule(this.rule);
-    let tree = this.token.getToken(document);
-
+    let tokenData = this.token.convert(document);
+    
+    this.tree.setRule(this.rule);
+    let treeData = this.tree.convert(tokenData);
+    
     this.render.setRules(this.rule);
-    // 将抽象语法树渲染回指定文本
-    // @ts-ignore
-    return this.render.render(tree as ITokenItem[]);
+    let doc = this.render.render([{
+      type: 'checked',
+      id: toolbox.generateId(),
+      data: '',
+      value: '',
+      children: treeData,
+      key: 'root',
+      isBlock: false,
+      isList: false,
+      isComple: true,
+    }]);
+    return doc;
   }
   /**
    * 将md转换为html
@@ -39,20 +55,43 @@ export class MdC {
   }
 }
 let doc = `
-# ccc
-## fff
+|标题1|标题1|
+|内容1|内容2|
+|内容3|内容4|
+
 [连接](ee/dedd/)
 ![图片](/sd/sd/fsa)
+1. ddd
+1. ddd
+1. 444ddsd
+# 标题1
+## 标题2
+### 标**题**3
+***yyyddd
+---dddd
 
-[aaa]ddddd(000)
-
-(--dddww)[ddd]
-
+**cccc**
+~~cccvvv~~
+` + '```' +
 `
+let cc = '222';
+console.log(cc);
+
+`+
+'```'
+
+// 333
+// 1. dddd
+// 1. dd344
+// 1. ddsl
+// # ccc
+// ## fff
 // let doc = `
 // [连接](ee/dedd/)
 // ![图片](/sd/sd/fsa)
-
+// ![图片](/sd/sd/fsa)
+// [aaa]ddddd(000)
+// (--dddww)[ddd]
 // [ddd]
 // (--dddww)
 // [aaa]ddddd(000)
--- src/utils/tools.ts
+++ src/utils/tools.ts
@@ -1,3 +1,5 @@
+import { ITokenItem } from "../core/iface";
+
 export let toolbox = {
   generateId () {
     return `${Math.floor(Math.random() * 1000)}-${Math.floor(Math.random() * 1000)}-${Math.floor(Math.random() * 1000)}`
@@ -5,5 +7,15 @@ export let toolbox = {
   dataFormat (d?: Date) {
     let date = d ? d : new Date();
     return `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+  },
+  getChildrenValue (item: ITokenItem) {
+    if (!item.children) {
+      return;
+    }
+    let str = '';
+    item.children?.forEach((i) => {
+      str += i.value;
+    })
+    return str;
   }
 }
